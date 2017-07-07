1c11674c496a86baff3618228cbeb9141bb6cd64
Maksym Komar <komar@evologics.de>
2017-07-07 19:20:36 +0200

    add to logger LOG_PREFIX

--- include/logger.h
+++ include/logger.h
@@ -11,13 +11,15 @@
 #define LOG_TRACE            (1 << 5)
 #define LOG_PERIODIC1        (1 << 6)
 #define LOG_PERIODIC2        (1 << 7)
+#define LOG_PREFIX           (1 << 8)
 
 void logger_init(uint16_t log_level_);
+int logger_(uint16_t log_level, const char *msg, ...);
 
 #define logger(level,fmt, ...)            \
     do {                                  \
         if (level & log_level)            \
-            printf(fmt, ## __VA_ARGS__);  \
+            logger_(level, fmt, ## __VA_ARGS__); \
     } while(0)
 
 # define logger_dump(level, buf, len)                          \
