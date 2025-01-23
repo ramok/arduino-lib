b2e9e86c4ca9ecf4f98c906df1ea36ebf5fbba9f
Maksym Komar <komar@evologics.de>
2025-01-23 15:58:26 +0100

    Merge

--- include/uart.h
+++ include/uart.h
@@ -6,9 +6,11 @@
 void uart_init(uint32_t serial_speed, bool empty_line);
 void uart_handle();
 
+#ifdef UART_INPUT
 #define UART_INPUT_BUF_LEN 30
 int uart_char_avail();
 int uart_get_line(char *buf);
+#endif
 
 
 #endif
