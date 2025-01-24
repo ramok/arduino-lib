4e665144509da75fa8507f4b0c28e8ca2eceb342
Maksym Komar <komar@evologics.de>
2025-01-24 20:38:25 +0100

    [uart] Add echo and editing uart input for a shell
    "Editing" only backspace supported now :)

--- include/uart.h
+++ include/uart.h
@@ -3,8 +3,8 @@
 
 #include <arduino.h>
 
-void uart_init(uint32_t serial_speed);
-void uart_handle(void);
+void uart_init(uint32_t serial_speed, bool empty_line);
+void uart_handle();
 
 #define UART_INPUT_BUF_LEN 30
 int uart_char_avail();
--- src/uart.cpp
+++ src/uart.cpp
@@ -8,6 +8,7 @@ static FILE uart_io = {0};
 static char uart_rx_buf[UART_INPUT_BUF_LEN + 1];
 static int  uart_rx_buf_len = 0;
 static bool uart_rx_buf_ready = false;
+static bool uart_rx_empty_line = false;
 
 static int uart_getchar(FILE *stream)
 {
@@ -18,24 +19,43 @@ int uart_char_avail() {
     return Serial.available();
 }
 
-void uart_handle(void)
+void uart_handle()
 {
     while (Serial.available()) {
         char c = Serial.read();
         if (c == '\r' || c == '\n') {
             uart_rx_buf[uart_rx_buf_len] = 0;
-            if (uart_rx_buf_len != 0)
+            if (uart_rx_buf_len != 0 || uart_rx_empty_line)
                 uart_rx_buf_ready = true;
+#ifdef UART_INPUT_EDIT
+            Serial.write("\r\n");
+#endif
         } else {
-            if (uart_rx_buf_len < UART_INPUT_BUF_LEN)
-                uart_rx_buf[uart_rx_buf_len++] = c;
+            if (uart_rx_buf_len < UART_INPUT_BUF_LEN) {
+                switch (c) {
+                    case 0x7f: /* backspace */
+                        if (uart_rx_buf_len > 0) {
+                            uart_rx_buf_len--;
+#ifdef UART_INPUT_EDIT
+                            Serial.write("\b \b");
+#endif
+                        }
+                        break;
+                    default:
+                        uart_rx_buf[uart_rx_buf_len++] = c;
+#ifdef UART_INPUT_EDIT
+                        Serial.write(c) ;
+#endif
+                        break;
+                }
+            }
         }
     }
 }
 
 int uart_get_line(char *buf)
 {
-    int rc = 0;
+    int rc = -1;
     if (uart_rx_buf_ready) {
         memcpy(buf, uart_rx_buf, uart_rx_buf_len);
         buf[uart_rx_buf_len] = 0;
@@ -72,11 +92,12 @@ static int uart_putchar(char c, FILE *stream)
     return 0 ;
 }
 
-void uart_init(uint32_t serial_speed)
+void uart_init(uint32_t serial_speed, bool empty_line = false)
 {
     Serial.begin(serial_speed);
     fdev_setup_stream (&uart_io, uart_putchar, uart_getchar, _FDEV_SETUP_RW);
     stderr = stdout = stdin = &uart_io;
+    uart_rx_empty_line = empty_line;
 }
 
 
